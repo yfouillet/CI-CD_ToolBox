47ee92c79c31651e82f85f331b5d141de8a6216a
Yoann Fouillet <yoann@fouillet.pro>
2019-03-06 15:09:33 +0100

    d
    Former-commit-id: 99c110e445d461cffbc0b514cb67f395984e2161

--- Terraform/test/main.tf
+++ Terraform/test/main.tf
@@ -1,7 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
-
-  region     = "us-east-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
 }
 
 module "vpc" {
---
+++ Terraform/test/variables.tf
@@ -0,0 +1,3 @@
+aws_access_key = "ami-abc123"
+aws_secret_key = "ami-def456"
+aws_region = ""
