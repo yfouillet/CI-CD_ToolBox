2f57685faf6ffc5d8315b333e84e2f24f467a7c2
Yoann Fouillet <yoann@fouillet.pro>
2019-03-07 09:03:00 +0000

    Updated main.tf
    Former-commit-id: 519ef1f01ac4b25dfb5c86677badc02c477bf473

--- Ansible/role/Cloud_agents/template/main.tf
+++ Ansible/role/Cloud_agents/template/main.tf
@@ -1,9 +1,13 @@
+{% if aws == true %}
+
 # Configure the AWS Provider
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
+
+{% endif %}
 
 
 module "vpc" {
