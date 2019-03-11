69238bd7a48b6fcb08840e94724ef1adfc759336
Yoann Fouillet <yoann@fouillet.pro>
2019-03-11 10:35:10 +0000

    Updated main.tf
    Former-commit-id: a9cf167372196108d9be5fd95bf18899d8d5a143

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
