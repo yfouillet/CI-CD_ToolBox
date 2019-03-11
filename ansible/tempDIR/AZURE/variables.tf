fca1a0d37dc9b1a45e95672f8cecc199e885c7e7
Yoann Fouillet <yoann@fouillet.pro>
2019-03-11 18:54:19 +0000

    Deleted tempDIR
    Former-commit-id: 203d51d26f6df2fc06a3b07260a64fed6a34e84a

--- ansible/tempDIR/AZURE/variables.tf
+++
@@ -1,9 +0,0 @@
-
-aws_bucket_tfstate = "test"
-aws_region_tfstate = "us-east"
-aws_bucketkey_tfstate = "Master-terraform/terraform.tfstate"
-
-azurerm_subscription_id = "test"
-azurerm_client_id       = "test"
-azurerm_client_secret   = "test"
-azurerm_tenant_id       = "test"
--- ansible/tempDIR/GOOGLE/variables.tf
+++
@@ -1,8 +0,0 @@
-
-aws_bucket_tfstate = "test"
-aws_region_tfstate = "us-east"
-aws_bucketkey_tfstate = "Master-terraform/terraform.tfstate"
-
-google_credentials = "${file("google_account.json")}"
-google_project     = "test"
-google_region      = "test"
