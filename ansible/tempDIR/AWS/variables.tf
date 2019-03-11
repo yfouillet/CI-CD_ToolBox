d83ecb856300b861a99e15bc1c189cb8d35e58f3
Yoann Fouillet <yoann@fouillet.pro>
2019-03-11 19:47:08 +0100

    h
    Former-commit-id: 22414f0c2cc7a1df542c712b055c5c109dedd9fa

---
+++ ansible/tempDIR/AWS/variables.tf
@@ -0,0 +1,8 @@
+
+aws_bucket_tfstate = "test"
+aws_region_tfstate = "us-east"
+aws_bucketkey_tfstate = "Master-terraform/terraform.tfstate"
+
+aws_access_key = "test"
+aws_secret_key = "test"
+aws_region = "test"
