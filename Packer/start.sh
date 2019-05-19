6a0ec7fdab118db9ad9e6e50643648e9c1cc2c3b
Yoann Fouillet <yoann@fouillet.pro>
2019-05-19 15:36:40 +0200

    clean project
    Former-commit-id: d0dd9abd5cc6bd7ad0b781e2a6c9e897a9a2e601

--- Packer/start.sh
+++ Packer/start.sh
@@ -5,7 +5,6 @@ echo "Start"
 if [ -L "/dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1" ]; # true if /your/file does not exist
 then
 echo "file exists, continuing"
-sudo mkdir /mnt/traefik-acme
 sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1
 sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/traefik-acme
 sudo touch /mnt/traefik-acme/acme.json
